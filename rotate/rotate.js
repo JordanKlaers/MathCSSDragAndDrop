967f040ea9686f873cd40db03ea9821915d93c36
Jordan Klaers <jordan.klaers@gmail.com>
2017-07-23 16:46:44 -0600

    dynamically rotats, stop rotating when dropped, and floats to the center of the drop zone

--- rotate/rotate.js
+++ rotate/rotate.js
@@ -34,24 +34,15 @@ $(document).ready(function() {
         var theDegree = Math.asin(numberForSin) * (180 / Math.PI)
         //  theDegree = (180-theDegree)+ 180;
         if (grabThisCenter.x < dropZoneCenter.x && grabThisCenter.y> dropZoneCenter.y) {
-            //does nothing to the degree
             theDegree = theDegree - 180
-            console.log("dropped to the  bottom left");
-            console.log(theDegree, 'the degree');
         } else if (grabThisCenter.x > dropZoneCenter.x && grabThisCenter.y> dropZoneCenter.y) {
             theDegree = -Math.abs(theDegree) - 180 ;
-            // theDegree = theDegree + (theDegree-180);
-            console.log("dropped to the bottom right");
-            console.log(theDegree, 'the degree');
         }
         else if(grabThisCenter.x > dropZoneCenter.x && grabThisCenter.y< dropZoneCenter.y){
-          console.log("dropped to the top right");
-          console.log(theDegree, 'the degree');
+          //do nothing
         }
         else if(grabThisCenter.x < dropZoneCenter.x && grabThisCenter.y< dropZoneCenter.y){
           theDegree = -Math.abs(theDegree) ;
-          console.log("dropped to the top right");
-          console.log(theDegree, 'the degree');
         }
 
 
@@ -62,11 +53,55 @@ $(document).ready(function() {
 
     }
     rotate();
-setInterval(rotate, 10);
+var interval = window.setInterval(rotate, 10);
     $("#grabThis").draggable({
         stop: function(event, ui) {
-            rotate();
-        }
-    })
+          window.clearInterval(interval)
+          // log
+              //                                                this is the grabbed object
+
+
+                  var offset = $("#dropZone").offset();         //offset gets the x and y coordinates
+                  var height = $("#dropZone").outerHeight();
+                  var width = $("#dropZone").outerWidth();
+                  var dropZoneCenter = {                         //get the height and width so you can move it up and over half those values to position the center
+                    x: offset.left + width / 2,
+                    y: offset.top + height / 2
+                  }
+                  console.log("dropzonecenter", dropZoneCenter);
+
+
+                  var Goffset = $("#grabThis").offset();         // gatta figure out where i dropped it
+                  var Gheight = $("#grabThis").outerHeight();
+                  var Gwidth = $("#grabThis").outerWidth();
+                  var grabCenter = {                              // shifting over  to  get the coordinates of its center
+                    x: Goffset.left + Gwidth / 2,
+                    y: Goffset.top + Gheight / 2
+                  }
+
+
+                  var distanceOBJ = {                           //gets the x distance and y distance between what you dropped and where it should have landed
+                    x: Math.abs(dropZoneCenter.x - grabCenter.x),
+                    y: Math.abs(dropZoneCenter.y - grabCenter.y)
+                  }
+                  distanceOBJ.x *= distanceOBJ.x
+                  distanceOBJ.y *= distanceOBJ.y
+                  var cSquared = distanceOBJ.x + distanceOBJ.y
+                  var distance= Math.sqrt(cSquared)        //pythagorean theroy to find the linear distance away
+                  
+
+                  if(distance<100){
+                    $("#grabThis").parent().css({position: 'relative'})
+                    var top = dropZoneCenter.y - Gwidth/2
+                    var left = dropZoneCenter.x - Gheight/2
+                    $("#grabThis").animate({top: top, left: left}, 1000, function(){
+                      console.log("done moving");
+                    })
+                  }
+              }
+          });
+
+
+
 
 });
